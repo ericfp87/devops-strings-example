fa81de09a65d313c3efd7c4270299781ef9ba68b
Eric Ferreira <eric_fpereira@hotmail.com>
2023-06-24 20:59:00 -0300

    adds linting

--- src/string_stadardization/stringstd.py
+++ src/string_stadardization/stringstd.py
@@ -1,9 +1,12 @@
 from unidecode import unidecode
 
+
 def standardize(text):
     return unidecode(text)
 
+
 name = "Éric Ferreira"
 std_name = standardize(name)
+
 
 print(f"Seu nome é {name}. Seu nome sem acento é {std_name}")
--- test/test_stringstd.py
+++ test/test_stringstd.py
@@ -1,5 +1,6 @@
 from string_stadardization import stringstd
 
+
 def test_full_name():
     name = "Eric Ferreira"
     std_name = stringstd.standardize(name)
